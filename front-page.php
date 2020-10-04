eed72070e55f265461903d8cb2228b31809efad8
haribooooom <haribooooom@gmail.com>
2020-10-04 16:12:54 +0900

    front-page

--- front-page.php
+++ front-page.php
@@ -1,182 +1,185 @@
 <?php get_header(); ?>
 
-  <div id="fv" class="bg-img bg">
+<div id="fv" class="bg-img bg">
 
-      <div class="box tcenter js-anime">
-      <strong class="txt">ちょっと自分が<br><span class="red">スキ</span>になる！</strong><strong >ようこそ、<span>ホメポジ</span>へ！</strong></div>
-      <!--box-->
-      <img class="js-anime" src="<?php echo get_template_directory_uri(); ?>/assets/img/il_fv_having_fun.png">
-
+  <div class="box tcenter js-anime">
+    <strong class="txt">ちょっと自分が<br><span class="red">スキ</span>になる！</strong><strong>ようこそ、<span>ホメポジ</span>へ！</strong>
   </div>
-  <div id="about">
-    <div class="inner">
+  <!--box-->
+  <img class="js-anime" src="<?php echo get_template_directory_uri(); ?>/assets/img/il_fv_having_fun.png">
 
-      <div class="bg-img bg">
-        <div class="box"><strong>最近、<br>褒められていますか？</strong>
-          <p>ホメポジは<br><span class="bold">「参加前よりちょっと自分を<span class="red">スキ</span>になる」</span><br>を目的としてお互いに頑張ったことを認め合う会です。</p>
-          <ul class="mb40">
-            <li class="js-anime">頑張ってるのに報われない</li>
-            <li class="js-anime">なんか毎日惰性で生きてる</li>
-            <li class="js-anime">優しい気持ちになりたい</li>
-            <li class="js-anime">知らない人と話してみたい</li>
-            <li class="js-anime">人見知りを克服したい</li>
-          </ul>
-          <p >そのようなあなたに来て欲しいと思っています。</p>
-          <div class="btn"><a href="./hoge.html">開催者の想い</a></div>
-        </div>
-        <!--box-->
+</div>
+<div id="about">
+  <div class="inner">
+
+    <div class="bg-img bg">
+      <div class="box"><strong>最近、<br>褒められていますか？</strong>
+        <p>ホメポジは<br><span class="bold">「参加前よりちょっと自分を<span class="red">スキ</span>になる」</span><br>を目的としてお互いに頑張ったことを認め合う会です。</p>
+        <ul class="mb40">
+          <li class="js-anime">頑張ってるのに報われない</li>
+          <li class="js-anime">なんか毎日惰性で生きてる</li>
+          <li class="js-anime">優しい気持ちになりたい</li>
+          <li class="js-anime">知らない人と話してみたい</li>
+          <li class="js-anime">人見知りを克服したい</li>
+        </ul>
+        <p>そのようなあなたに来て欲しいと思っています。</p>
+        <div class="btn"><a href="./hoge.html">開催者の想い</a></div>
       </div>
-      <!--wrap-->
-
+      <!--box-->
     </div>
-    <!--inner-->
+    <!--wrap-->
+
   </div>
+  <!--inner-->
+</div>
 
 <div id="voice">
-    <div class="inner">
+  <div class="inner">
 
-      <h2>参加した人の声</h2>
-      <div class="wrap pc-flex">
+    <h2>参加した人の声</h2>
+    <div class="wrap pc-flex">
 
-        <div class="swiper-voice-container mb20">
-          <div class="swiper-wrapper">
-            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_way_conf.png" alt="">
-              <div class="txt">
-                <p >（ルイさん）</p>
-                <p >「人見知りを治したい」,「明るい人と会いたい」という人に本気でおすすめです。最高に素敵な会です。</p>
-              </div>
-              <!--box-->
+      <div class="swiper-voice-container mb20">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_way_conf.png" alt="">
+            <div class="txt">
+              <p>（ルイさん）</p>
+              <p>「人見知りを治したい」,「明るい人と会いたい」という人に本気でおすすめです。最高に素敵な会です。</p>
             </div>
-            <!--.swiper-slide-->
+            <!--box-->
           </div>
-          <!--swiper-wrapper-->
+          <!--.swiper-slide-->
         </div>
-        <!--swiper-container-->
-
-        <div class="box">
-          <div class="txt">何かしらのいい感じの言葉</div>
-        </div>
-
+        <!--swiper-wrapper-->
       </div>
-      <!--voice-wrap-->
+      <!--swiper-container-->
+
+      <div class="box">
+        <div class="txt">何かしらのいい感じの言葉</div>
+      </div>
 
     </div>
-    <!--inner-->
-  </div>
-  <div id="entry">
-    <div class="inner">
-        <h2>参加方法</h2>
-        <div class="pc-flex">
-          <div class="item js-anime"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_way_conf.png" alt="">
-            <p >後述の参加上の注意を確認する</p>
-          </div>
-          <!--entryitem-->
-          <div class="item js-anime"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_way_message.png" alt="">
-            <p >Twitter・当公式サイトの応募フォームから応募！</p>
-          </div>
-          <!--entryitem-->
-          <div class="item js-anime"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_way_wait.png" alt="">
-            <p >運営からの連絡を待つ！</p>
-          </div>
-          <!--entryitem-->
-        </div>
-        <!--entrybox flex-->
-    </div>
-    <!--inner-->
-  </div>
-  <div id="attention">
-    <div class="inner">
-      <h2>参加上の注意事項</h2>
-        <div class="bg-img bg">
-          <ul class="mb40">
-            <li class="js-anime">相手をけなしたりバカにすること</li>
-            <li class="js-anime">宗教、ビジネスの勧誘</li>
-            <li class="js-anime">店員さんや周囲のお客様への迷惑行為</li>
-            <li class="js-anime">他の参加者に対して迷惑やハラスメントに当たる行為</li>
-            <li class="js-anime">その他、不適切と取れるような行為</li>
-          </ul>
-          <p class="bold ">※守れない場合は退場していただく場合もあります。</p>
-        </div>
-        <!--box-->
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_caution_warning.png" alt="">
+    <!--voice-wrap-->
 
-    </div>
-    <!--inner-->
   </div>
+  <!--inner-->
+</div>
+<div id="entry">
+  <div class="inner">
+    <h2>参加方法</h2>
+    <div class="pc-flex">
+      <div class="item js-anime"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_way_conf.png" alt="">
+        <p>後述の参加上の注意を確認する</p>
+      </div>
+      <!--entryitem-->
+      <div class="item js-anime"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_way_message.png" alt="">
+        <p>Twitter・当公式サイトの応募フォームから応募！</p>
+      </div>
+      <!--entryitem-->
+      <div class="item js-anime"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_way_wait.png" alt="">
+        <p>運営からの連絡を待つ！</p>
+      </div>
+      <!--entryitem-->
+    </div>
+    <!--entrybox flex-->
+  </div>
+  <!--inner-->
+</div>
+<div id="attention">
+  <div class="inner">
+    <h2>参加上の注意事項</h2>
+    <div class="bg-img bg">
+      <ul class="mb40">
+        <li class="js-anime">相手をけなしたりバカにすること</li>
+        <li class="js-anime">宗教、ビジネスの勧誘</li>
+        <li class="js-anime">店員さんや周囲のお客様への迷惑行為</li>
+        <li class="js-anime">他の参加者に対して迷惑やハラスメントに当たる行為</li>
+        <li class="js-anime">その他、不適切と取れるような行為</li>
+      </ul>
+      <p class="bold ">※守れない場合は退場していただく場合もあります。</p>
+    </div>
+    <!--box-->
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/il_caution_warning.png" alt="">
 
-  <div id="qa">
-    <div class="inner">
+  </div>
+  <!--inner-->
+</div>
+
+<div id="qa">
+  <div class="inner">
 
     <h2>ホメポジQ&amp;A</h2>
 
-        <div class="items mb40">
-          <div class="cp_qa">
-            <div class="cp_actab ">
-              <input id="qa_1" type="checkbox" name="tabs">
-              <label for="qa_1">Q1.ホメポジはいつ開催されますか？</label>
-              <div class="cp_actab-content">
-                <p>A１.月一で不定期で運営の揃う土日の夜に開催されます。
-                  次回の開催についてはホメポジ公式アカウントか、運営のツイートを随時ご確認ください。<a href="#">→ホメポジ公式</a></p>
-              </div>
-            </div>
-            <div class="cp_actab "><input id="qa_2" type="checkbox" name="tabs"><label for="qa_2">Q2.どこで開催されますか？</label>
-              <div class="cp_actab-content">
-                <p>A2.東京都内の山手線沿線で開催されます。</p>
-              </div>
-            </div>
-            <div class="cp_actab "><input id="qa_3" type="checkbox" name="tabs"><label for="qa_3">Q3.一人で参加することが不安です。。</label>
-              <div class="cp_actab-content">
-                <p>A3.一人で参加される方がほとんどです！安心して参加してください。（下記の「参加者のあれこれ」というページから今までの参加者の方の傾向を紹介するページがあります。よろしければご覧ください！）</p>
-              </div>
-            </div>
-            <div class="cp_actab "><input id="qa_4" type="checkbox" name="tabs"><label for="qa_4">Q3.一人で参加することが不安です。。</label>
-              <div class="cp_actab-content">
-                <p>A3.一人で参加される方がほとんどです！安心して参加してください。（下記の「参加者のあれこれ」というページから今までの参加者の方の傾向を紹介するページがあります。よろしければご覧ください！）</p>
-              </div>
-            </div>
-            <div class="cp_actab "><input id="qa_5" type="checkbox" name="tabs"><label for="qa_5">Q3.一人で参加することが不安です。。</label>
-              <div class="cp_actab-content">
-                <p>A3.一人で参加される方がほとんどです！安心して参加してください。（下記の「参加者のあれこれ」というページから今までの参加者の方の傾向を紹介するページがあります。よろしければご覧ください！）</p>
-              </div>
-            </div>
+    <div class="items mb40">
+      <div class="cp_qa">
+
+         <div class="cp_actab ">
+          <input id="qa_1" type="checkbox" name="tabs">
+          <label for="qa_1">Q1.ホメポジはいつ開催されますか？</label>
+          <div class="cp_actab-content">
+            <p>A１.月一で不定期で運営の揃う土日の夜に開催されます。
+              次回の開催についてはホメポジ公式アカウントか、運営のツイートを随時ご確認ください。<a href="#">→ホメポジ公式</a></p>
           </div>
         </div>
-        <!--items-->
 
-
-        <div class="btn__items tcenter">
-          <p class="mb30">その他ご質問等ございましたら、<br>
-            <a href="#">ホメポジ公式アカウント</a>までご連絡ください。</p>
-
-          <!--watch__btn-->
-        </div>
-        <!--btn__items-->
-
-    </div>
-    <!--inner-->
-  </div>
-  <div id="f-mes">
-    <div class="inner">
-
-          <div class="txt tcenter">
-            <p class="mes"><span class="bold">ホメポジ</span>に参加して</p>
-            <p class="mes">ちょっと自分が<br><span class="red">スキ</span><br>になる</p>
-            <p >あなたのご参加をお待ちしております！</p>
+        <div class="cp_actab "><input id="qa_2" type="checkbox" name="tabs"><label for="qa_2">Q2.どこで開催されますか？</label>
+          <div class="cp_actab-content">
+            <p>A2.東京都内の山手線沿線で開催されます。</p>
           </div>
-          <!--f-mes__box-->
-
-            <div class="swiper-fmes-container">
-              <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/00.jpg" alt="">
-                </div>
-                <!--swiper-slide-->
-              </div>
-              <!--swiper-wrapper-->
-            </div>
-            <!--swiper-container-->
-
+        </div>
+        <div class="cp_actab "><input id="qa_3" type="checkbox" name="tabs"><label for="qa_3">Q3.一人で参加することが不安です。。</label>
+          <div class="cp_actab-content">
+            <p>A3.一人で参加される方がほとんどです！安心して参加してください。（下記の「参加者のあれこれ」というページから今までの参加者の方の傾向を紹介するページがあります。よろしければご覧ください！）</p>
+          </div>
+        </div>
+        <div class="cp_actab "><input id="qa_4" type="checkbox" name="tabs"><label for="qa_4">Q3.一人で参加することが不安です。。</label>
+          <div class="cp_actab-content">
+            <p>A3.一人で参加される方がほとんどです！安心して参加してください。（下記の「参加者のあれこれ」というページから今までの参加者の方の傾向を紹介するページがあります。よろしければご覧ください！）</p>
+          </div>
+        </div>
+        <div class="cp_actab "><input id="qa_5" type="checkbox" name="tabs"><label for="qa_5">Q3.一人で参加することが不安です。。</label>
+          <div class="cp_actab-content">
+            <p>A3.一人で参加される方がほとんどです！安心して参加してください。（下記の「参加者のあれこれ」というページから今までの参加者の方の傾向を紹介するページがあります。よろしければご覧ください！）</p>
+          </div>
+        </div>
+      </div>
     </div>
-    <!--inner-->
+    <!--items-->
+
+    <div class="btn__items tcenter">
+      <p class="mb30">その他ご質問等ございましたら、<br>
+        <a href="#">ホメポジ公式アカウント</a>までご連絡ください。
+      </p>
+
+      <!--watch__btn-->
+    </div>
+    <!--btn__items-->
+
   </div>
+  <!--inner-->
+</div>
+<div id="f-mes">
+  <div class="inner">
+
+    <div class="txt tcenter">
+      <p class="mes"><span class="bold">ホメポジ</span>に参加して</p>
+      <p class="mes">ちょっと自分が<br><span class="red">スキ</span><br>になる</p>
+      <p>あなたのご参加をお待ちしております！</p>
+    </div>
+    <!--f-mes__box-->
+
+    <div class="swiper-fmes-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/00.jpg" alt="">
+        </div>
+        <!--swiper-slide-->
+      </div>
+      <!--swiper-wrapper-->
+    </div>
+    <!--swiper-container-->
+
+  </div>
+  <!--inner-->
+</div>
 <?php get_footer(); ?>
